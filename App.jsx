df1da654ef91c50d0c39a6a0e7164f2c5bbc17be
raadchfat <rod@omahadrain.com>
2025-07-14 12:57:26 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import KPIDashboard from "./KPIDashboard";
+
+function App() {
+  return <KPIDashboard />;
+}
+export default App;
---
+++ KPIDashboard.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useCallback, useMemo } from 'react';
+import { Upload, FileText, AlertCircle, CheckCircle, TrendingUp, Star, Clock, Wrench, DollarSign, Zap } from 'lucide-react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const KPIDashboard = () => {
+  // ... All your code as above (truncated for space) ...
+};
+
+export default KPIDashboard;
